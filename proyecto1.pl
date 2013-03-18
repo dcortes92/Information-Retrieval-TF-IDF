f67d342b7f6bf0676b938c9e1256c23fc54e9737
Daniel Cortes <dcortes92@hotmail.com>
2013-03-18 16:06:14 -0600

    Borrado archivo prueba prueba

--- proyecto1.pl
+++ proyecto1.pl
@@ -1,22 +1,65 @@
-#!/usr/bin/perl
+ use File::Basename;
 
-    use strict;
-    use warnings;
+sub open_dir{
+	my ($path) = ($_[0]);
+	opendir(DIR, $path) or die $!; #se abre el directorio
+	my @files = grep(!/^\./,readdir(DIR));
+	closedir(DIR);
+	foreach $file (@files){
+		$file = $path.'/'.$file; #path absoluto del fichero o directorio
+		next unless( -f $file or -d $file ); #se rechazan pipes, links, etc ..
+	   	if( -d $file){
+			open_dir($file,$hash);
+		}else{
+	   		#print $file."\n";
+	   		if($file =~ /\.id$/){
+	   			&abrir_archivo($file);
+	   		}
+		}		
+	}
+}
 
-    my $dir = "man.es";
+sub abrir_archivo{
+	my ($path) = ($_[0]);
+	open (MYFILE, $path);
+	@terminos;
+	while (<MYFILE>) {
+		chop();
+		$linea = $_;	
+		print $linea."\n";
+		#$linea =~ tr/A-Z/a-z/;	
+		#$linea =~ tr/·ÈÌÛ˙¸¡…Õ”⁄‹/aeiouuaeiouu/;
+		$linea =~ s/-\n//g;	
 
-    opendir(DIR, $dir) or die $!;
+		@palabras = split (' ', $linea);
+		$largo = @palabras;
 
-    while (my $file = readdir(DIR)) {
+		for ($i = 0; $i < $largo; $i++) {
+			#print $palabras[$i]."\n";
+			$temporal = $palabras[$i];
+			if (!($temporal =~ /^[0-9]+$/)){
+				push(@terminos, $temporal);
+				$terminos{$temporal}++;
+				#print $temporal,"\n";
+			}
+		}
 
-        # We only want files
-        next unless (-f "$dir/$file");
 
-        # Use a regular expression to find files ending in .txt
-        next unless ($file =~ m/\.txt$/);
+	}
+	close (MYFILE);
+	
+	$largo = @terminos;
+	@sorted = sort {$b <=> $a} @terminos;
+	$primero = $sorted[0];
+	print "primero ".$primero."\n";
+	
+	open (NUEVO, '>>data.txt');
+	print NUEVO $path.";".fileparse($path).";".$largo.";".$terminos{$primero}.";";
+		foreach $palabra (sort keys (%terminos)) {
+		   print NUEVO "(".$palabra.",".$terminos{$palabra}.");";
+		}
+	print NUEVO "\n";
+	close (NUEVO); 
+}
 
-        print "$file\n";
-    }
-
-    closedir(DIR);
-    exit 0;
+&open_dir("/home/isaac/TEC/RIT/Proyecto 1");
